e2652a3739105e5790a4a2019e3b34d3f6797d4b
Alexander <36491066+danube88@users.noreply.github.com>
2021-08-30 23:54:21 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,7 +123,8 @@
                     </div>
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) Timezone: {{ config('app.timezone') }}
+                        {{ Illuminate\Support\Carbon::now() }} Locale: ({{ App::getLocale() }})
                     </div>
                 </div>
             </div>
